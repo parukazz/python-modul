18d95c808c9ab996c0a48c199487583332b76962
parukrev <xavierrevian@gmail.com>
2024-08-13 13:29:05 +0700

    casting variables module

---
+++ casting_variables.py
@@ -0,0 +1,21 @@
+# Specify a Variables Type
+
+"""
+  Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+"""
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x, y, z)
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x, y, z)
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x, y, z)
