edb1c42d2828371cfc35b2b37ef649c7500d3efc
parukrev <xavierrevian@gmail.com>
2024-08-14 14:36:30 +0700

    update module list

--- lists.py
+++ lists.py
@@ -34,3 +34,69 @@ print(list1)
 
 thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
 print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist) # replace banana with blackcurrent and watermelon
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
