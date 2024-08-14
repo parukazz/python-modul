5e9ff2f31a269aaced39357aa5b84203a1a47377
parukrev <xavierrevian@gmail.com>
2024-08-14 10:10:23 +0700

    operator module

---
+++ operators.py
@@ -0,0 +1,43 @@
+# Operators
+
+# Operators are used to perform operations on variables and values
+
+print(10 + 5)
+
+# Arithmetic Operators
+
+"""
+| Operators |   Name          |  Examples  |
+| --------- | --------------- | ---------- |
+| +         | Addition        | x + y      |
+| -         | Substraction    | x - y      |
+| *         | Multiplication  | x * y      |
+| /         | Division        | x / y      |
+| %         | Modulus         | x % y      |
+| **        | Exponentiation  | x ** y     |
+| //        | Floor division  | x // y     |
+"""
+
+# Operator Precedence
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
+
+"""
+Operator	  Description	
+()	        Parentheses	
+**	        Exponentiation	
++x  -x  ~x	Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	Multiplication, division, floor division, and modulus	
++  -	      Addition and subtraction	
+<<  >>	    Bitwise left and right shifts	
+&	          Bitwise AND	
+^	          Bitwise XOR	
+|	          Bitwise OR	
+==  !=  >   is  is not  in  not in 	Comparisons, identity, and membership operators
+>=  <  <=  	
+not	        Logical NOT	
+and	        AND	
+or	        OR
+"""
+
+print(5 + 4 - 7 + 3)
