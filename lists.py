8664890b456016e513a0a011ad5d5c7da5c8e72e
parukrev <xavierrevian@gmail.com>
2024-08-15 15:16:03 +0700

    add List Comprehension & The Syntax

--- lists.py
+++ lists.py
@@ -100,3 +100,32 @@ while i < len(thislist):
 
 thislist = ["apple", "banana", "cherry"]
 [print(x) for x in thislist]
+
+# List Comprehension
+
+# List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+# Example:
+# Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
+
+# Without list comprehension you will have to write a for statement with a conditional test inside:
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x) # mengambil data dari list fruits lalu mensortir data yang hanya ada huruf a saja di dalam nya, lalu dimasukkan lagi ke tempat kosong yaitu newlist
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+# The Syntax
+"""
+newlist = [expression for item in iterable if condition == True]
+"""
