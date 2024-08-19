2d113826f579ded14dd1e744e1a077bb8fd12d73
parukrev <xavierrevian@gmail.com>
2024-08-19 14:09:59 +0700

    oop modules

---
+++ oop/classes.py
@@ -0,0 +1,11 @@
+class Item:
+  pass
+
+item1 = Item() # this is object
+# add atribute with dot
+item1.name = "Phone" 
+item1.price = 100
+item1.quantity = 5
+
+print(item1.name)
+print(item1)
---
+++ oop/description.md
@@ -0,0 +1,3 @@
+# Object-oriented programming (OOP)
+
+a method of structuring a program by bundling related properties and behaviors into individual objects.
